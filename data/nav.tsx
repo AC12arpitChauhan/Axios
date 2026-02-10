28ed758ece2539a504197d09e3cd37a2e6603fc2
Rafat Alam <Rafat.Alam.RA@gmail.com>
2026-02-10 10:40:43 +0530

    nav confict arises

--- data/nav.tsx
+++ data/nav.tsx
@@ -1,8 +1,8 @@
 import { FaInfoCircle, FaCalendarAlt, FaUsers, FaEnvelope } from "react-icons/fa";
 
 export const navItems = [
-  { name: "About", link: "#about", icon: <FaInfoCircle /> },
-  { name: "Events", link: "#events", icon: <FaCalendarAlt /> },
+  { name: "About", link: "/#about", icon: <FaInfoCircle /> },
+  { name: "Events", link: "/#events", icon: <FaCalendarAlt /> },
   { name: "Team", link: "/team", icon: <FaUsers /> },
-  { name: "Contact", link: "#contact", icon: <FaEnvelope /> },
+  { name: "Contact", link: "/#contact", icon: <FaEnvelope /> },
 ];
