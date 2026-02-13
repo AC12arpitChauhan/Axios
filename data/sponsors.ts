d9d9b0db9551c9107c49edc3a3fb4ade82f24e7c
TjRajpoot <tanuj9340685860@gmail.com>
2026-02-13 18:16:13 +0530

    Update sponsors and UI

--- data/sponsors.ts
+++ data/sponsors.ts
@@ -5,31 +5,67 @@ export const companies: Company[] = [
   {
     id: 1,
     name: "cloudinary",
-    img: "/cloud.svg",
-    nameImg: "/cloudName.svg",
+    img: "/sponsers/cloud.svg",
+    nameImg: "/sponsers/cloudName.svg",
   },
   {
     id: 2,
     name: "appwrite",
-    img: "/app.svg",
-    nameImg: "/appName.svg",
+    img: "/sponsers/app.svg",
+    nameImg: "/sponsers/appName.svg",
   },
   {
     id: 3,
     name: "HOSTINGER",
-    img: "/host.svg",
-    nameImg: "/hostName.svg",
+    img: "/sponsers/host.svg",
+    nameImg: "/sponsers/hostName.svg",
   },
   {
     id: 4,
     name: "stream",
-    img: "/s.svg",
-    nameImg: "/streamName.svg",
+    img: "/sponsers/s.svg",
+    nameImg: "/sponsers/streamName.svg",
   },
   {
     id: 5,
     name: "docker.",
-    img: "/dock.svg",
-    nameImg: "/dockerName.svg",
+    img: "/sponsers/dock.svg",
+    nameImg: "/sponsers/dockerName.svg",
+  },
+  {
+    id: 6,
+    name: "AWS",
+    img: "/sponsers/aws-svgrepo-com-2.svg", // Assuming this is the Icon
+    nameImg: "/sponsers/aws-svgrepo-com.svg", // Using same for now as no separate name SVG found
+  },
+  {
+    id: 7,
+    name: "Devfolio",
+    img: "/sponsers/devfolio.svg",
+    nameImg: "/sponsers/devfolio.svg",
+  },
+  {
+    id: 8,
+    name: "Ethindia",
+    img: "/sponsers/ETHIndia.png",
+    nameImg: "/sponsers/ETHIndia.png",
+  },
+  {
+    id: 9,
+    name: "Careers360",
+    img: "/sponsers/idGBuOks9O_logos.svg", // Best candidate based on filename
+    nameImg: "/sponsers/idGBuOks9O_logos.svg",
+  },
+  {
+    id: 10,
+    name: "Cafe Amado",
+    img: "/sponsers/590936225_17861191914555643_8146110666959980631_n.jpg", // Placeholder - likely the JPG
+    nameImg: "/sponsers/590936225_17861191914555643_8146110666959980631_n.jpg",
+  },
+  {
+    id: 11,
+    name: "House of Thrill",
+    img: "/sponsers/cropped-logo-white-website-scaled-1.png.webp", // Placeholder - likely the WebP
+    nameImg: "/sponsers/cropped-logo-white-website-scaled-1.png.webp",
   },
 ];
